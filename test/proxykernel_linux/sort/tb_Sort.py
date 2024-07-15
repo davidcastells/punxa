af716952de2a81a14d17c3ac7c75571d2c6cd2d1
David Castells-Rufas <david.castells@uab.cat>
2024-07-15 15:38:24 +0200

    memory layout

--- test/proxykernel_linux/sort/tb_Sort.py
+++ test/proxykernel_linux/sort/tb_Sort.py
@@ -25,7 +25,7 @@ from punxa.bus import *
 from punxa.uart import *
 from punxa.clint import *
 from punxa.plic import *
-from punxa.single_cycle.singlecycle_processor_proxy_kernel import *
+from punxa.single_cycle.singlecycle_processor_proxy_linux import *
 from punxa.instruction_decode import *
 from punxa.interactive_commands import *
     
@@ -132,258 +132,6 @@ def restore(filename=ex_dir + 'checkpoint.dat'):
     ser.close()
 
 
-def run(upto, maxclks=100000, verbose=True, autoCheckpoint=False):
-    import time
-    global print
-    global dummy_print
-
-    
-    if not(verbose):
-        cpu.setVerbose(False)
-                        
-    sim = hw.getSimulator()
-
-    t0 = time.time()
-    clk0 = sim.total_clks
-
-    t0 = time.time()
-    clk0 = sim.total_clks
-    
-    count = 0
-    istart = cpu.csr[0xC02]
-    ilast = istart
-    
-    while (cpu.pc != upto):
-        sim.clk(1)
-        count += 1
-        icur = cpu.csr[0xC02]
-        
-        if not(sim.do_run):
-            break;
-        if (count > maxclks):
-            break;
-        if ((icur % 10000 == 0) and (icur != ilast)):
-            print('ins: {:n}'.format(icur))
-            ilast = icur
-            
-    if (cpu.pc != upto):
-        print('did not reach address')
-
-        if (sim.do_run and autoCheckpoint):
-            print('auto checkpointing')
-            checkpoint()
-
-    if not(verbose):
-        cpu.setVerbose(True)
-
-    tf = time.time()
-    clkf = sim.total_clks
-
-    if (tf != t0):    
-        freq = (clkf-clk0)/(tf-t0)
-    else:
-        freq = '?'
-
-    print('clks: {} time: {} simulation freq: {}'.format(clkf-clk0, tf-t0, freq))
-        
-
-def regs():
-    print('pc: {:016X}'.format(cpu.pc))
-    for i in range(8):
-        print('r{:2}={:016X}  |  r{:2}={:016X}  |  r{:2}={:016X}  |  r{:2}={:016X} '.format(
-            i, cpu.reg[i], i+8, cpu.reg[i+8], i+16, cpu.reg[i+16], i+24, cpu.reg[i+24]))
-            
-    for i in range(8):
-        print('fr{:2}={:016X}  |  fr{:2}={:016X}  |  fr{:2}={:016X}  |  fr{:2}={:016X} '.format(
-            i, cpu.freg[i], i+8, cpu.freg[i+8], i+16, cpu.freg[i+16], i+24, cpu.freg[i+24]))
-        
-## used when upgrading stack from address to address+time
-# def fix_stack():
-#     newstack = []
-#     for i in cpu.stack:
-#         newstack.append((i,0))
-        
-#     cpu.stack = newstack
-    
-def stack():
-    for idx, finfo in enumerate(cpu.stack):
-        
-        #f = cpu.getPhysicalAddressQuick(finfo[0])
-        f = finfo[0]    # no need to translate, since symbols are provided in 
-                        # virtual memory addresses for kernel
-        
-        if (f in cpu.funcs.keys()):
-            print(' '*idx, cpu.funcs[f])
-        else:
-            print(' '*idx, '{:016X}'.format(f))
-                  
-        
-def dump(address, size=0x100):
-    pos = address 
-    for i in range((size+15)//16):
-        sline = ''
-        print('{:016X}:|'.format(pos), end='')
-        for j in range(16):
-            value = memory.readByte(pos-mem_base)
-            print('{:02X}'.format(value), end='')
-            if (value >= 32 and value < 127):
-                sline += chr(value)
-            else:
-                sline += '·'
-            pos += 1
-            
-        print('| "{}"'.format(sline))
-#    memory.write(32*4+0x00, 0xfe010113) # addi    sp,sp,-32
-#    memory.write(32*4+0x04, 0x00112e23) # sw      ra,28(sp)
-
-
-
-def get_va_parts(v):
-    ret = {}
-    ret['vpn2'] = (v >> 30) & ((1<<9)-1)
-    ret['vpn1'] = (v >> 21) & ((1<<9)-1)
-    ret['vpn0'] = (v >> 12) & ((1<<9)-1)
-    ret['offset'] = v & ((1<<12)-1)
-    return ret
-            
-def pageTables(root=None, vbase = 0, level=2, printPTE=True):
-    """
-    Traverses the page tables from the provided root page table.
-    The R flag indicates that the PTE is a leaf.
-    
-    Parameters
-    ----------
-    root : TYPE, optional
-        DESCRIPTION. The default is None.
-    vbase : TYPE, optional
-        DESCRIPTION. The default is 0.
-    level : TYPE, optional
-        DESCRIPTION. The default is 2.
-    printPTE : TYPE, optional
-        DESCRIPTION. The default is True.
-
-    Returns
-    -------
-    The number of valid page tables .
-
-    """
-    if (root is None):
-        v = cpu.csr[0x180] # satp
-        mode = (v >> 60) & ((1<<4)-1)
-        smode = ['Base', '','','','','','','','Sv39','Sv48','Sv57','Sv64'][mode]
-        asid = (v >> 44) & ((1<<16)-1)
-        print('Virtual Memory Mode: {} {} ASID: {:04X}'.format(mode, smode, asid))
-        root = (v & ((1<<44)-1)) << 12
-        
-        if (root == 0):
-            return 0
-    
-    indent = ''
-    for i in range(2-level): indent += ' '
-    
-    if (level == 2):
-        tableName = 'Root'
-    else:
-        tableName = 'Table'
-    print('{}{}: {:08X}'.format(indent, tableName, root))
-    
-    totalTables = 1
-    
-    for i in range(0, 512, 1):
-        add = root+i*8
-        v = memory.read_i64(add-mem_base)
-        ppn2 = (v >> 28) & ((1<<26)-1)
-        ppn1 = (v >> 19) & ((1<<9)-1)
-        ppn0 = (v >> 10) & ((1<<9)-1)
-        rsw = (v >> 8) & ((1<<2)-1)
-        D = [' ','D'][(v >> 7) & 1]
-        A = [' ','A'][(v >> 6) & 1]
-        G = [' ','G'][(v >> 5) & 1]
-        U = [' ','U'][(v >> 7) & 1]
-        X = [' ','X'][(v >> 3) & 1]
-        W = [' ','W'][(v >> 2) & 1]
-        R = [' ','R'][(v >> 1) & 1]
-        valid = v & 1
-        
-        va = vbase + (1 << [12,21,30][level]) * i
-                
-        phy = ppn2 << 30 | ppn1 << 21 | ppn0 << 12
-        
-        if (valid):
-            if (printPTE):
-                print('{} {:3d} ppn2:{:08X} ppn1:{:03X} ppn0:{:03X} rsw:{:0} '
-                      '{}{}{}{}{}{}{} va: {:016X} pa: {:016X}'.format(
-                          indent,  i, ppn2, ppn2, ppn0, rsw, 
-                          D,A,G,U,X,W,R,
-                          va, phy))
-            
-            if (X == ' ' and W == ' ' and R == ' '):
-                totalTables += pageTables(phy, va, level-1, printPTE)
-    
-    return totalTables
-
-def memoryMap():
-    for i in range(len(bus.start)):
-        size = bus.stop[i] - bus.start[i]
-        units = 'B'
-        if (size > 1024):
-            size = size/1024
-            units = 'KiB'
-        if (size > 1024):
-            size = size/1024
-            units = 'MiB'
-        if (size > 1024):
-            size = size/1024
-            units = 'GiB'
-        
-        print('* {:016X} - {:016X} {:.0f} {}'.format(bus.start[i], bus.stop[i], size, units))
-        
-        if (bus.start[i] == mem_base):
-            # we assume thereis a sparse-memory starting at memory area
-            # details on memory
-            for block in memory.area:
-                size = block[1]
-                units = 'B'
-                if (size > 1024):
-                    size = size/1024
-                    units = 'KiB'
-                if (size > 1024):
-                    size = size/1024
-                    units = 'MiB'
-                if (size > 1024):
-                    size = size/1024
-                    units = 'GiB'
-                print('  {:016X} - {:016X} {:.0f} {}'.format(mem_base + block[0], mem_base + block[0] + block[1] - 1, size, units))
-                #print('??', hex(block[0]), hex(block[1]))
-                
-def reallocMem(add, size):
-    memory.reallocArea(add - mem_base, size)
-    
-def findFunction(name):
-    for a in cpu.funcs.keys():
-        if (cpu.funcs[a] == name):
-            return a
-    return None
-
-#  +-----+    +-----+     +-----+
-#  | CPU |--C-| bus |--M--| mem |
-#  +-----+    |     |     +-----+
-#             |     |     +------+
-#             |     |--U--| uart |
-#             |     |     +------+
-#             |     |     +------+
-#             |     |--P--| PLIC |
-#             |     |     +------+
-#             |     |     +-------+
-#             |     |--L--| CLINT |
-#             |     |     +-------+
-#             +-----+
-#  | start          | stop           | device        |
-#  | 0000 0000 0000 | 0001 BFEF FFFF | memory (5GB)  |
-#  | 0000 BFF0 0000 | 0002 8000 0000 | pmem (3GB)    |
-#  | 00FF F0C2 C000 | 00FF F0C2 CFFF | uart          |
-#  | 00FF F102 0000 | 00FF F102 FFFF | CLINT         |
-#  | 00FF F110 0000 | 00FF F11F FFFF | PLIC          |
 
 def buildHw():
     global memory
@@ -402,7 +150,7 @@ def buildHw():
 
     memory = SparseMemory(hw, 'main_memory', mem_width, 32, port_m, mem_base=mem_base)
 
-    #memory.reallocArea(0, 1 << 16)
+    memory.reallocArea(0, 1 << 24)
 
     #test = ISATestCommunication(hw, 'test', mem_width, 8, port_t)
 
@@ -437,7 +185,7 @@ def buildHw():
                                           (port_p, 0xFFF1100000),
                                           (port_l, 0xFFF1020000)])
 
-    cpu = SingleCycleRISCVProxyKernel(hw, 'RISCV', port_c, int_soft, int_timer, ext_int_targets, mem_base)
+    cpu = SingleCycleRISCVProxyLinux(hw, 'RISCV', port_c, int_soft, int_timer, ext_int_targets, 0)
 
     cpu.min_clks_for_trace_event = 1000
     cpu.behavioural_memory = memory
@@ -446,6 +194,7 @@ def buildHw():
     import punxa.interactive_commands
     punxa.interactive_commands._ci_hw = hw
     punxa.interactive_commands._ci_cpu = cpu
+    punxa.interactive_commands._ci_bus = bus
     
     return hw
 
@@ -474,7 +223,7 @@ def prepareTest(test_file, args):
     programFile = ex_dir + test_file
     
 
-    memory.reallocArea(0x10000, 0x80000)
+    #memory.reallocArea(0x10000, 0x80000)
 
     loadElf(memory, programFile, 0 )     
     loadSymbolsFromElf(cpu,  programFile, mem_base) 
@@ -484,13 +233,13 @@ def prepareTest(test_file, args):
 
     cpu.pc = start_adr
     
-    stack_base = 0x90000
+    stack_base = 0x100000
     stack_size = 0X50000
     cpu.reg[2] = mem_base + stack_base + stack_size - 8
 
     memory.reallocArea(stack_base, stack_size)
 
-    cpu.heap_base = 0x100000
+    cpu.heap_base = 0x150000
     cpu.heap_size = 0x040000 
 
     memory.reallocArea(cpu.heap_base, cpu.heap_size)
@@ -499,24 +248,29 @@ def prepareTest(test_file, args):
     print(f'\tStack base: 0x{stack_base:016X} size: 0x{stack_size:016X}')
     print(f'\tHeap base:  0x{cpu.heap_base:016X} size: 0x{cpu.heap_size:016X}')
 
-    # Now push the arguments to the stack
+    # Now push the arguments to the stack (in reverse order?)
+    args = []
     args = [programFile] + args
     argc = len(args)
     argsp = [0] * argc
-
+    
+    
     for i in range(argc):
         param = args[argc-1-i]
-        pushString(param)
+        cpu.pushString(param)
         argsp[i] = cpu.reg[2] + 1
-
-    pushInt64(0)
-
+    
+    for i in range(4):
+        cpu.pushInt64(0) # for other libc argmuments
+        
+    cpu.pushInt64(0)
+    
     for i in range(argc):
         add = argsp[i]
-        pushInt64(add)
-
-    pushInt64(argc)
-
+        cpu.pushInt64(add)
+    
+    cpu.pushInt64(argc)
+    
     cpu.reg[2] += 1
     
 
@@ -542,11 +296,7 @@ def runTest():
 
 def prepare():
     prepareTest('sort.elf', ['-v' ])
-    #step(10000000)
-    #print()
-    #print('Console Output')
-    #print('-'*80)
-    #console()
+    
 
 if __name__ == "__main__":
     print(sys.argv)
