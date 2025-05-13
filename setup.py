f31832b1690654d95b2ec405f963ed1c738a23af
David Castells-Rufas <david.castells@uab.cat>
2025-05-13 15:45:28 +0200

    version and dependencies

--- setup.py
+++ setup.py
@@ -13,15 +13,15 @@ with open("README.md", "r") as fh:
                          
 setup(
     name='py4hw',
-    version='0.2',
+    version='2025.1',
     author='David Castells-Rufas',
     author_email='david.castells@uab.cat',
     description='Python-based RISC-V Full System Simulator.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/davidcastells/punxa',
-    install_requires=['py4hw>=2024.3', 'pyelftools', 'itanium-demangler'],
-    tests_require=['py4hw>=2024.3', 'pyelftools', 'itanium-demangler'],
+    install_requires=['py4hw>=2025.4', 'pyelftools', 'itanium-demangler'],
+    tests_require=['py4hw>=2025.4', 'pyelftools', 'itanium-demangler'],
     packages=find_packages(),
     package_data={'': ['*.png']}
 )
