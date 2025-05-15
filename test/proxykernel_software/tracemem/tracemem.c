f710d0c80125a9070a8ed2e9623aeb7d4fbea9d6
David Castells-Rufas <david.castells@uab.cat>
2025-05-15 06:45:18 +0200

    factorial computation

--- test/proxykernel_software/tracemem/tracemem.c
+++ test/proxykernel_software/tracemem/tracemem.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int ga = 0xCAFEBABE;
 int gb;
 
@@ -18,30 +21,53 @@ int gb;
 
 #define TRACEMEM(arg0, arg1, arg2) ECALL(0xFF, arg0, arg1, arg2)
 
-void f(int fa, int* fb)
+int fact(int fa, int* fb)
 {
-    TRACEMEM(&fa, "fa", sizeof(int))
-    TRACEMEM(&fb, "fb", sizeof(int*))
+    int ret;
+    
+    // trick to print fa1, fa2, ...
+    char sfa[] = {'f', 'a', '0'+*fb, 0};
+    char sfb[] = {'f', 'b', '0'+*fb, 0};
+        
+    TRACEMEM(&fa, sfa, sizeof(int))
+    TRACEMEM(&fb, sfb, sizeof(int*))
+    
+    if (fa == 1)
+        return 1;
+        
+    else 
+    {
+        *fb += 1;
+        ret = fact(fa -1, fb);
+    }
 }
 
 void last();
 
 int main(int argc, char* args[])
 {
-    int la=2;
+    // I play the trick of having a dummy function at the end (last) to estimate
+    // the size of the code    
+    TRACEMEM(fact, "code", (int)last - (int)fact)
+    
+    int la=5;
     
     int* ph = malloc(sizeof(int) * 2);
+    ph[0] = 0;
     
+    // Trace global variable locations
     TRACEMEM(&ga, "ga", sizeof(int))
     TRACEMEM(&gb, "gb", sizeof(int))
+    
+    // Trace local variables in this function scope
+    
     TRACEMEM(&la, "la", sizeof(int))
-    TRACEMEM(&ph, "ph", sizeof(int*))
-    TRACEMEM(ph, "h", sizeof(int)*2)
+    TRACEMEM(&ph, "ph", sizeof(int*)) // ph is a pointer
+    TRACEMEM(ph, "h", sizeof(int)*2)  // I name h the value pointed by ph (which should be in the HEAP)
     
-    f(la, ph);
+    ph[1] = fact(la, ph);
     
-    TRACEMEM(f, "code", (int)last - (int)f)
-    
+    printf("Factorial of %d = %d. Took %d iterations\n", la, ph[1], ph[0]);
 }
 
 void last()
